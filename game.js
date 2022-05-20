63c954b6744e91676b5fddf7a46ea143b111bab4
Schmoodled <sonphan1242@gmail.com>
2022-05-20 13:55:56 -0700

    Update playRound function
    The function now compares user input to randomized computer
    selection.

--- game.js
+++ game.js
@@ -20,9 +20,21 @@ function computerPlay(choice) {
 function playRound (playerSelection, computerSelection) {
     playerSelection = prompt('Rock, Paper, Scissors -- Shoot!');
     playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
+    console.log('Your choice: ' + playerSelection);
     computerSelection = computerPlay();
-    console.log(computerSelection);
+    console.log('Opponent\'s choice: ' + computerSelection);
+    console.log(playerSelection == computerPlay);
+    //compare your choice to opponents
+    //draw
+    if (playerSelection === computerSelection) {
+        alert('Draw, try again. :)');
+    //rock vs paper, paper vs scissors, scissors vs rock result in a loss
+    } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock') {
+        alert('You lose, loser! :^)')
+    //else you win
+    } else {
+        alert('You win! :)')
+    }
 }
 
 playRound();
