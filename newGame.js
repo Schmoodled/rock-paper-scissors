fc087a3054e13344ec88a02acfb67b96f19adcb6
Schmoodled <sonphan1242@gmail.com>
2023-06-15 15:17:07 -0700

    Comment pseudocode for playRound function

--- newGame.js
+++ newGame.js
@@ -1,11 +1,26 @@
+//randomly selects rock, paper, or scissor for the computer
 function getComputerChoice () {
     //sets a random interger between 0 - 2
     let randomNumber = Math.floor(Math.random() * 3);
     console.log(randomNumber);
-    return randomNumber == 0 ? 'rock' : randomNumber == 1 ? 'paper' : 'scissor';
+    return randomNumber == 0 ? 'rock' 
+           : randomNumber == 1 ? 'paper' 
+           : 'scissor';
 }
 
-// let message = (login == 'Employee') ? 'Hello' : 
-// (login == 'Director') ? 'Greetings' :
-// (login = '') ? 'No Login' : '';
 console.log (getComputerChoice());
+
+//plays one round of rock-paper-scissors
+function playRound (playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt('Rock Paper Scissors -- Shoot!'); 
+    //clicking cancel or escape ==  null | enter == ''
+    console.log(playerSelection);
+    //if null prompt user 'Are you sure you want to end the game?'
+    //if '' ask user to input a string
+    //else toLowerCase
+    //check string for rock, paper, or scissors
+    //if string is rock paper or scissors compare to computer choice
+}
+
+playRound();
